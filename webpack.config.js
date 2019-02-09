ef2b0280120507cd7e9aa6fef7cf0fd5a8177b8b
RomanBY <vasikroman@mail.ru>
2019-02-09 23:51:51 +0300

    bugs fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     entry: './src/app.js',
@@ -10,8 +11,42 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader', ]
-            }
+                use: /*['style-loader', 'css-loader', 'sass-loader'],*/
+                    [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    autoprefixer({
+                                        browsers:['ie >= 8', 'last 4 version']
+                                    })
+                                ],
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+
+                        }
+                    ],
+
+
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png)$/,
+                exclude: /node_modules/,
+                loader:'url-loader?limit=1024&name=src/img/[name].[ext]'
+            },
+
         ]
     }
 };
